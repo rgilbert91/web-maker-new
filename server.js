e17c916531f2c96fd18ec8f0773be706e5620800
Rasheem Gilbert <rasheem.gilbert@gmail.com>
2019-11-26 11:22:34 -0500

    send server code

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 // import express library
 const express = require("express");
+// importing path to be used on heroku
+const path = require("path");
 
 // init express
 const app = express();
 
-// create a server listening at localhost:3100
-app.listen(3100, () => {
-  console.log("Server started on port 3100");
+app.use(express.json());
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 3100;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
