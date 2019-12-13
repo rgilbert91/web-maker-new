8531e81210f5eb8bc2d0661c7bd5aa41ed837bcc
Rasheem Gilbert <rasheem.gilbert@gmail.com>
2019-12-13 13:52:06 -0500

    Mongo Website data

---
+++ models/Website.js
@@ -0,0 +1,9 @@
+// Import Mongoose
+const mongoose = require("mongoose");
+
+const WebsiteSchema = mongoose.Schema({
+  developerId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  name: { type: String, required: true },
+  description: { type: String }
+});
+module.exports = mongoose.model("website", WebsiteSchema);
--- routes/websites.js
+++ routes/websites.js
@@ -1,99 +1,39 @@
 const express = require("express");
 const router = express.Router();
-
-const websites = [
-  {
-    _id: "123",
-    name: "Facebook",
-    developerId: "456",
-    description: "Lorem"
-  },
-  {
-    _id: "234",
-    name: "Twitter",
-    developerId: "456",
-    description: "Lorem"
-  },
-  {
-    _id: "456",
-    name: "Msimbo",
-    developerId: "456",
-    description: "Lorem"
-  },
-  {
-    _id: "890",
-    name: "Go",
-    developerId: "123",
-    description: "Lorem"
-  },
-  {
-    _id: "567",
-    name: "Tic Tac Toe",
-    developerId: "123",
-    description: "Lorem"
-  },
-  {
-    _id: "678",
-    name: "Checkers",
-    developerId: "123",
-    description: "Lorem"
-  },
-  {
-    _id: "789",
-    name: "Chess",
-    developerId: "234",
-    description: "Lorem"
-  }
-];
+const Website = require("../models/Website");
 
 // create websites /api/webistes.
-router.post("/", (req, res) => {
-  const newWebsite = req.body;
-  websites.push(newWebsite);
-  res.json(newWebsite);
+router.post("/", async (req, res) => {
+  const newWebsite = new Website({ ...req.body });
+  const website = await newWebsite.save();
+  res.json(website);
 });
 
 // Find all websites by given user id
-router.get("/user/:uid", (req, res) => {
+router.get("/user/:uid", async (req, res) => {
   const uid = req.params.uid;
-  const currentWebsites = [];
-  for (let i = 0; i < websites.length; i++) {
-    if (websites[i].developerId === uid) {
-      currentWebsites.push(websites[i]);
-    }
-  }
+  const currentWebsites = await Website.find({ developerId: uid });
   res.json(currentWebsites);
 });
 
 // Find website by ID
-router.get("/:wid", (req, res) => {
+router.get("/:wid", async (req, res) => {
   const wid = req.params.wid;
-  let website = null;
-  for (let i = 0; i < websites.length; i++) {
-    if (websites[i]._id === wid) {
-      website = websites[i];
-    }
-  }
+  let website = await Website.findById(wid);
   res.json(website);
 });
 
 // update website
-router.put("/", (req, res) => {
-  const newWeb = req.body;
-  for (let i = 0; i < websites.length; i++) {
-    if (websites[i]._id === newWeb._id) {
-      websites[i] = newWeb;
-    }
-  }
-  res.json(newWeb);
+router.put("/", async (req, res) => {
+  const newWebsite = req.body;
+  await Website.findByIdAndUpdate(newWebsite._id, newWebsite);
+  res.json(newWebsite);
 });
 
 // Delete website
-router.delete("/:wid", (req, res) => {
+router.delete("/:wid", async (req, res) => {
   const wid = req.params.wid;
-  for (let i = 0; i < websites.length; i++) {
-    websites.splice(i, 1);
-  }
+  await Website.findByIdAndRemove(wid);
   res.json(websites);
 });
 
